900bbce180bc58d80526088ca61fad02997ae12e
Jacqueline930 <jacqueline.lopez.930@gmail.com>
2019-03-28 13:12:32 -0600

    created a server file to run local host

---
+++ server/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var data = require('../database/data');
+
+
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/../client/dist'));
+
+app.listen(3000, function() {
+  console.log('Server started and listening on port 3000');
+});
