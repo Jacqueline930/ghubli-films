7a1a53a9f94268051007ecd1ee2251035beb477e
Beagbarreto <beagbarreto09@gmail.com>
2019-04-04 14:51:23 -0600

    able to get API info in the console

--- client/src/components/MovieSearch.jsx
+++ client/src/components/MovieSearch.jsx
@@ -1,39 +1,13 @@
 import React from "react";
-import axios from "axios";
 
-class MovieSearch extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      title: ""
-    }
-  }
-
-  handleTitle() {
-    this.setState({
-      title: event.target.value
-    })
-  }
-
-  handleSubmit() {
-    axios.get("https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49").then(function (response) {
-      return response.json();
-    }).then(function (responseData) {
-      _this.setState({
-        data: responseData
-      });
-    });
-  };
-
-
-  render(){
+const MovieSearch = (props) => {
     return (
-      <div className="Search">
-        <input type="text" placeholder="Search for Movie..."></input>
-        <button onClick={this.handleSubmit}> Search </button>
-      </div>
+      <form onSubmit={props.getTitleMovie}>
+        <input style={{margin:"20px auto", display:"block"}}
+               type="text" name="movieTitle" placeholder="Search for Movie..."/>
+             <button> Search </button>
+      </form>
     )
-  }
 }
 
 
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -8,20 +8,37 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      movieList: []
+      title: []
     }
+    this.getTitleMovie = this.getTitleMovie.bind(this)
   }
 
+  componentDidMount(){
+    axios.get(`https://ghibliapi.herokuapp.com/films/Castle-in-the-Sky`)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch(err => console.error(err))
+  }
+
+  getTitleMovie(e) {
+    // e.preventDefault();
+    // const movie = e.target.elements.movieTitle.value;
+    axios.get(`https://ghibliapi.herokuapp.com/films/`)
+      .then((res) => {
+        console.log(res);
+      })
+  }
 
   render() {
     return(
       <div>
         <div className="App">
           <header className="App-header">
-            <h1 className= "App-title">Ghubli Films</h1>
+            <h1 className="App-title">Ghubli Films</h1>
           </header>
         </div>
-        <MovieSearch />
+        <MovieSearch getTitleMovie={this.getTitleMovie} />
       </div>
     )
   }
